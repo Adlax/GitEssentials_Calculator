6af1aa8ec41622f43f43e7d680127206f17e9dc2
collaborator <h.videlier@gmail.com>
2022-11-06 13:36:56 +0100

    Multiply feature added

--- CalculatorNew.java
+++ CalculatorNew.java
@@ -3,7 +3,7 @@ public class Calculator {
     public static void main(string[] args) {}
 
     public int addition(int a,int b) {
-        return a+b;
+        return a + b;
     }
 
     public double addition(double a, double b) {
@@ -15,7 +15,15 @@ public class Calculator {
     }
 
     public double substractrion(double a, double b) {
-        return a -b;
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double multiply(double a, double b) {
+        return a * b;
     }
     
 }
