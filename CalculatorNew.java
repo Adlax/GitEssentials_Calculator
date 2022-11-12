685845d697ef8bc2e9d2b0228e3ee847826c3c27
Adlax <h.videlier@gmail.com>
2022-11-12 18:48:33 +0100

    Revert "Revert "Division added""
    This reverts commit eb535aafce1ec1eefa6af087fe7bb614cb0bae9e.

--- CalculatorNew.java
+++ CalculatorNew.java
@@ -30,6 +30,10 @@ public class Calculator {
     public double multiply(double a, double b) {
         return a * b;
     }
+
+    public double divide(double a, double b) {
+        return b / a;
+    }
     
 }
 
